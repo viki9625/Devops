11360e985fc1af6da4081235bdfd0d448bf5e13d
viki9625 <boostupvikram@gmail.com>
2025-09-01 20:37:35 +0530

    Images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,113 +1,132 @@
 $(document).ready(function () {
+  $("#menu").click(function () {
+    $(this).toggleClass("fa-times");
+    $(".navbar").toggleClass("nav-toggle");
+  });
 
-    $('#menu').click(function () {
-        $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle');
+  $(window).on("scroll load", function () {
+    $("#menu").removeClass("fa-times");
+    $(".navbar").removeClass("nav-toggle");
+
+    if (window.scrollY > 60) {
+      document.querySelector("#scroll-top").classList.add("active");
+    } else {
+      document.querySelector("#scroll-top").classList.remove("active");
+    }
+
+    // scroll spy
+    $("section").each(function () {
+      let height = $(this).height();
+      let offset = $(this).offset().top - 200;
+      let top = $(window).scrollTop();
+      let id = $(this).attr("id");
+
+      if (top > offset && top < offset + height) {
+        $(".navbar ul li a").removeClass("active");
+        $(".navbar").find(`[href="#${id}"]`).addClass("active");
+      }
     });
+  });
 
-    $(window).on('scroll load', function () {
-        $('#menu').removeClass('fa-times');
-        $('.navbar').removeClass('nav-toggle');
+  // smooth scrolling
+  $('a[href*="#"]').on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top,
+      },
+      500,
+      "linear"
+    );
+  });
 
-        if (window.scrollY > 60) {
-            document.querySelector('#scroll-top').classList.add('active');
-        } else {
-            document.querySelector('#scroll-top').classList.remove('active');
-        }
-
-        // scroll spy
-        $('section').each(function () {
-            let height = $(this).height();
-            let offset = $(this).offset().top - 200;
-            let top = $(window).scrollTop();
-            let id = $(this).attr('id');
-
-            if (top > offset && top < offset + height) {
-                $('.navbar ul li a').removeClass('active');
-                $('.navbar').find(`[href="#${id}"]`).addClass('active');
-            }
-        });
-    });
-
-    // smooth scrolling
-    $('a[href*="#"]').on('click', function (e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top,
-        }, 500, 'linear')
-    });
-
-    // <!-- emailjs to mail contact form data -->
+  // <!-- emailjs to mail contact form data -->
+  // Run after page loads
+  $(document).ready(function () {
     $("#contact-form").submit(function (event) {
-        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
+      event.preventDefault(); // stop refresh
 
-        emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
-            .then(function (response) {
-                console.log('SUCCESS!', response.status, response.text);
-                document.getElementById("contact-form").reset();
-                alert("Form Submitted Successfully");
-            }, function (error) {
-                console.log('FAILED...', error);
-                alert("Form Submission Failed! Try Again");
-            });
-        event.preventDefault();
+      // ✅ Put your Public Key here (from EmailJS dashboard → Account → API Keys)
+      emailjs.init("jOYIST1HCfm1oImAh");
+
+      // ✅ Use your actual service ID + template ID
+      emailjs
+        .sendForm("service_ohotszx", "template_gp2fjnb", "#contact-form")
+        .then(
+          function (response) {
+            console.log("SUCCESS!", response.status, response.text);
+            $("#contact-form")[0].reset();
+            alert("✅ Form Submitted Successfully!");
+          },
+          function (error) {
+            console.log("FAILED...", error);
+            alert("❌ Form Submission Failed! Try Again");
+          }
+        );
     });
-    // <!-- emailjs to mail contact form data -->
+  });
 
+  // <!-- emailjs to mail contact form data -->
 });
 
-document.addEventListener('visibilitychange',
-    function () {
-        if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Vikram Roy";
-            $("#favicon").attr("href", "assets/images/favicon.png");
-        }
-        else {
-            document.title = "Come Back To Portfolio";
-        }
-    });
-
+document.addEventListener("visibilitychange", function () {
+  if (document.visibilityState === "visible") {
+    document.title = "Portfolio | Vikram Roy";
+    $("#favicon").attr("href", "assets/images/favicon.png");
+  } else {
+    document.title = "Come Back To Portfolio";
+  }
+});
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["DevOps Engineer", "Server Managing", "Cloud Engineer", "AutoMating Things", "Tech Enthusiast", "Problem Solver",],
-    loop: true,
-    typeSpeed: 50,
-    backSpeed: 25,
-    backDelay: 500,
+  strings: [
+    "DevOps Engineer",
+    "Server Managing",
+    "Cloud Engineer",
+    "AutoMating Things",
+    "Tech Enthusiast",
+    "Problem Solver",
+  ],
+  loop: true,
+  typeSpeed: 50,
+  backSpeed: 25,
+  backDelay: 500,
 });
 // <!-- typed js effect ends -->
 
 async function fetchData(type = "skills") {
-    let response
-    type === "skills" ?
-        response = await fetch("skills.json")
-        :
-        response = await fetch("./projects/projects.json")
-    const data = await response.json();
-    return data;
+  let response;
+  type === "skills"
+    ? (response = await fetch("skills.json"))
+    : (response = await fetch("./projects/projects.json"));
+  const data = await response.json();
+  return data;
 }
 
 function showSkills(skills) {
-    let skillsContainer = document.getElementById("skillsContainer");
-    let skillHTML = "";
-    skills.forEach(skill => {
-        skillHTML += `
+  let skillsContainer = document.getElementById("skillsContainer");
+  let skillHTML = "";
+  skills.forEach((skill) => {
+    skillHTML += `
         <div class="bar">
               <div class="info">
                 <img src=${skill.icon} alt="skill" />
                 <span>${skill.name}</span>
               </div>
-            </div>`
-    });
-    skillsContainer.innerHTML = skillHTML;
+            </div>`;
+  });
+  skillsContainer.innerHTML = skillHTML;
 }
 
 function showProjects(projects) {
-    let projectsContainer = document.querySelector("#work .box-container");
-    let projectHTML = "";
-    projects.slice(0, 10).filter(project => project.category != "android").forEach(project => {
-        projectHTML += `
+  let projectsContainer = document.querySelector("#work .box-container");
+  let projectHTML = "";
+  projects
+    .slice(0, 10)
+    .filter((project) => project.category != "android")
+    .forEach((project) => {
+      projectHTML += `
         <div class="box">
       <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
       <div class="content">
@@ -122,43 +141,41 @@ function showProjects(projects) {
           </div>
         </div>
       </div>
-    </div>`
+    </div>`;
     });
-    projectsContainer.innerHTML = projectHTML;
+  projectsContainer.innerHTML = projectHTML;
 
-    // <!-- tilt js effect starts -->
-    VanillaTilt.init(document.querySelectorAll(".tilt"), {
-        max: 15,
-    });
-    // <!-- tilt js effect ends -->
+  // <!-- tilt js effect starts -->
+  VanillaTilt.init(document.querySelectorAll(".tilt"), {
+    max: 15,
+  });
+  // <!-- tilt js effect ends -->
 
-    /* ===== SCROLL REVEAL ANIMATION ===== */
-    const srtop = ScrollReveal({
-        origin: 'top',
-        distance: '80px',
-        duration: 1000,
-        reset: true
-    });
+  /* ===== SCROLL REVEAL ANIMATION ===== */
+  const srtop = ScrollReveal({
+    origin: "top",
+    distance: "80px",
+    duration: 1000,
+    reset: true,
+  });
 
-    /* SCROLL PROJECTS */
-    srtop.reveal('.work .box', { interval: 200 });
-
+  /* SCROLL PROJECTS */
+  srtop.reveal(".work .box", { interval: 200 });
 }
 
-fetchData().then(data => {
-    showSkills(data);
+fetchData().then((data) => {
+  showSkills(data);
 });
 
-fetchData("projects").then(data => {
-    showProjects(data);
+fetchData("projects").then((data) => {
+  showProjects(data);
 });
 
 // <!-- tilt js effect starts -->
 VanillaTilt.init(document.querySelectorAll(".tilt"), {
-    max: 15,
+  max: 15,
 });
 // <!-- tilt js effect ends -->
-
 
 // pre loader start
 // function loader() {
@@ -190,61 +207,59 @@ VanillaTilt.init(document.querySelectorAll(".tilt"), {
 // }
 
 // Start of Tawk.to Live Chat
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
-})();
-// End of Tawk.to Live Chat
-
+// var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+// (function () {
+//     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+//     s1.async = true;
+//     s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+//     s1.charset = 'UTF-8';
+//     s1.setAttribute('crossorigin', '*');
+//     s0.parentNode.insertBefore(s1, s0);
+// })();
+// // End of Tawk.to Live Chat
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
 const srtop = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 1000,
-    reset: true
+  origin: "top",
+  distance: "80px",
+  duration: 1000,
+  reset: true,
 });
 
 /* SCROLL HOME */
-srtop.reveal('.home .content h3', { delay: 200 });
-srtop.reveal('.home .content p', { delay: 200 });
-srtop.reveal('.home .content .btn', { delay: 200 });
+srtop.reveal(".home .content h3", { delay: 200 });
+srtop.reveal(".home .content p", { delay: 200 });
+srtop.reveal(".home .content .btn", { delay: 200 });
 
-srtop.reveal('.home .image', { delay: 400 });
-srtop.reveal('.home .linkedin', { interval: 600 });
-srtop.reveal('.home .github', { interval: 800 });
-srtop.reveal('.home .twitter', { interval: 1000 });
-srtop.reveal('.home .telegram', { interval: 600 });
-srtop.reveal('.home .instagram', { interval: 600 });
-srtop.reveal('.home .dev', { interval: 600 });
+srtop.reveal(".home .image", { delay: 400 });
+srtop.reveal(".home .linkedin", { interval: 600 });
+srtop.reveal(".home .github", { interval: 800 });
+srtop.reveal(".home .twitter", { interval: 1000 });
+srtop.reveal(".home .telegram", { interval: 600 });
+srtop.reveal(".home .instagram", { interval: 600 });
+srtop.reveal(".home .dev", { interval: 600 });
 
 /* SCROLL ABOUT */
-srtop.reveal('.about .content h3', { delay: 200 });
-srtop.reveal('.about .content .tag', { delay: 200 });
-srtop.reveal('.about .content p', { delay: 200 });
-srtop.reveal('.about .content .box-container', { delay: 200 });
-srtop.reveal('.about .content .resumebtn', { delay: 200 });
-
+srtop.reveal(".about .content h3", { delay: 200 });
+srtop.reveal(".about .content .tag", { delay: 200 });
+srtop.reveal(".about .content p", { delay: 200 });
+srtop.reveal(".about .content .box-container", { delay: 200 });
+srtop.reveal(".about .content .resumebtn", { delay: 200 });
 
 /* SCROLL SKILLS */
-srtop.reveal('.skills .container', { interval: 200 });
-srtop.reveal('.skills .container .bar', { delay: 400 });
+srtop.reveal(".skills .container", { interval: 200 });
+srtop.reveal(".skills .container .bar", { delay: 400 });
 
 /* SCROLL EDUCATION */
-srtop.reveal('.education .box', { interval: 200 });
+srtop.reveal(".education .box", { interval: 200 });
 
 /* SCROLL PROJECTS */
-srtop.reveal('.work .box', { interval: 200 });
+srtop.reveal(".work .box", { interval: 200 });
 
 /* SCROLL EXPERIENCE */
-srtop.reveal('.experience .timeline', { delay: 400 });
-srtop.reveal('.experience .timeline .container', { interval: 400 });
+srtop.reveal(".experience .timeline", { delay: 400 });
+srtop.reveal(".experience .timeline .container", { interval: 400 });
 
 /* SCROLL CONTACT */
-srtop.reveal('.contact .container', { delay: 400 });
-srtop.reveal('.contact .container .form-group', { delay: 400 });
+srtop.reveal(".contact .container", { delay: 400 });
+srtop.reveal(".contact .container .form-group", { delay: 400 });
